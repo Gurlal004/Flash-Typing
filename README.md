2a96dd3faa38253f54ce82eb01805c3d9de8d396
gurlalbadwal <gbad0004@student.monash.edu>
2025-10-04 18:08:17 +1000

    docker added

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Making a typing website using React Library
 
 Use: npm install  
 To make it work use: npm install vite --save-dev at client/flash-typing  
-Use: Remove-Item -Recurse -Force node_modules then npm install express  
+Use: Remove-Item -Recurse -Force node_modules then npm install express
+
+
+Docker commands. 
+- docker compose down --rmi all --volumes --remove-orphans. 
+- docker-compose up. 
+- docker-compose down. 
--- server/server.js
+++ server/server.js
@@ -1,51 +1,3 @@
-// const express = require("express")
-// const sqlite3 = require("sqlite3").verbose()
-// const cors = require("cors")
-// const app = express()
-// const path = require("path")
-// const fetch = require("node-fetch")
-
-// app.use(cors({
-//     origin: "*"
-// }));
-
-// const db = new sqlite3.Database(path.join(__dirname, "database.db"))
-
-// app.options("/api/words", (req, res) => {
-//   res.sendStatus(204);
-// });
-
-// app.get("/api/words", async (req, res) => {
-//     const source  = req.query.source;
-
-//     if(source === "api"){
-//         try{
-//             const response = await fetch("https://words.biebersprojects.com/words/100/");
-//             const data = await response.json();
-//             const wrapped = data.map(word => ({word}));
-//             console.log(wrapped.length);
-//             return res.json(wrapped);
-//         }catch(err){
-//             return res.status(500).json({error: "Failed to fetch words"})
-//         }
-//     }
-
-//     // const sql = "SELECT word FROM words ORDER BY RANDOM() LIMIT 5";
-
-//     // db.all(sql, [], (err, rows) => {
-//     //     if(err){
-//     //         return res.status(500).json({error: err.message});
-//     //     }
-//     //     res.json(rows);
-//     // });
-// });
-
-// const PORT = 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server started on port ${PORT}`);
-// })
-
-
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
@@ -70,10 +22,6 @@ app.get('/api/words', async (req, res) => {
 
     const data = await response.json();
 
-    // Print to console
-    // console.log('Fetched Data:', data);
-
-    // Send response to client
     res.json(data);
   } catch (error) {
     console.error('Error fetching words:', error);
